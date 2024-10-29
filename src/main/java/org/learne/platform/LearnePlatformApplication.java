246e2598dce0cebe4cba68e55e1193c7d20a45c0
Jorge <u201921734@upc.edu.pe>
2024-10-28 20:01:02 -0500

    chore: created base project

---
+++ src/main/java/org/learne/platform/LearnePlatformApplication.java
@@ -0,0 +1,13 @@
+package org.learne.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnePlatformApplication.class, args);
+    }
+
+}
