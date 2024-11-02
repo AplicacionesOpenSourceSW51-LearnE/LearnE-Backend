dbd4c07c69b91b77fb9f6268f785339fb31d9e85
Pedro Guia <u202212010@upc.edu.pe>
2024-11-01 22:35:11 -0500

    feat: created endpoint for sign in functionality

---
+++ src/main/java/org/learne/platform/learne/application/internal/queryservices/CourseQueryServiceImpl.java
@@ -0,0 +1,36 @@
+package org.learne.platform.learne.application.internal.queryservices;
+
+import org.learne.platform.learne.domain.model.aggregates.Course;
+import org.learne.platform.learne.domain.model.queries.GetAllCoursesQuery;
+import org.learne.platform.learne.domain.model.queries.GetCourseByIdQuery;
+import org.learne.platform.learne.domain.model.queries.GetLearnePathItemByCourseIdAndTeacherIdQuery;
+import org.learne.platform.learne.domain.services.CourseQueryService;
+import org.learne.platform.learne.infrastructure.persistence.jpa.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseQueryServiceImpl implements CourseQueryService {
+    public final CourseRepository courseRepository;
+
+    public CourseQueryServiceImpl(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    @Override
+    public List<Course> handle(GetAllCoursesQuery query) {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public Optional<Course> handle(GetCourseByIdQuery query) {
+        return courseRepository.findCourseBy(query.courseId());
+    }
+
+    @Override
+    public Optional<Course> handle(GetLearnePathItemByCourseIdAndTeacherIdQuery query) {
+        return Optional.empty();
+    }
+}
