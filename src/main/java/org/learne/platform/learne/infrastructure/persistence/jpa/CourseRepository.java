43b9d7280152c0dfe7b2fdeeaa2edb3f49d6f1ed
Pedro Guia <u202212010@upc.edu.pe>
2024-11-01 21:08:15 -0500

    feat: created endpoint for create user functionality

---
+++ src/main/java/org/learne/platform/learne/infrastructure/persistence/jpa/CourseRepository.java
@@ -0,0 +1,14 @@
+package org.learne.platform.learne.infrastructure.persistence.jpa;
+
+import org.learne.platform.learne.domain.model.aggregates.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, String>{
+    Optional<Course> findCourseBy(String title);
+
+    boolean existsCourseBy(String title);
+}
