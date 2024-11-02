cf427a22a19e5469b7d959a0788be4c8de1d7195
Pedro Guia <u202212010@upc.edu.pe>
2024-11-01 22:34:02 -0500

    feat: created course management endpoints

--- src/main/java/org/learne/platform/learne/domain/model/aggregates/Course.java
+++ src/main/java/org/learne/platform/learne/domain/model/aggregates/Course.java
@@ -2,15 +2,20 @@ package org.learne.platform.learne.domain.model.aggregates;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.Getter;
 import org.apache.logging.log4j.util.Strings;
+import org.learne.platform.learne.domain.model.commands.CreatedCourseCommand;
 import org.learne.platform.learne.domain.model.valueobjects.LearnePath;
 import org.learne.platform.learne.domain.model.valueobjects.TeacherId;
 import org.learne.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Course extends AuditableAbstractAggregateRoot<Course> {
+    private Long id;
     private String title;
     private String description;
     private TeacherId teacherId;
@@ -19,12 +24,6 @@ public class Course extends AuditableAbstractAggregateRoot<Course> {
     private String prior_knowledge;
     private String principal_image;
 
-    @Embedded
-    private final LearnePath learnePath;
-
-    /**
-     * Create a new course
-     */
 
     public Course() {
         this.title = Strings.EMPTY;
@@ -33,28 +32,18 @@ public class Course extends AuditableAbstractAggregateRoot<Course> {
         this.duration = Strings.EMPTY;
         this.prior_knowledge = Strings.EMPTY;
         this.principal_image = Strings.EMPTY;
-        this.learnePath = new LearnePath();
     }
 
-    /**
-     * Update the course information
-     * @param title The title of the course
-     * @param description The description of the course
-     * @param teacherId The teacher id about the course
-     * @param  level The level in the course
-     * @param duration The duration of time in the course
-     * @param prior_knowledge The course syllabus
-     * @param principal_image The image of the course
-     * @return
-     */
-    public Course updateInformation(String title, String description, TeacherId teacherId, String level, String duration, String prior_knowledge, String principal_image) {
-        this.title = title;
-        this.description = description;
-        this.teacherId = teacherId;
-        this.level = level;
-        this.duration = duration;
-        this.prior_knowledge = prior_knowledge;
-        this.principal_image = principal_image;
-        return this;
+    public Course(CreatedCourseCommand command) {
+        this.id = command.Id();
+        this.title = command.title();
+        this.description = command.description();
+        this.teacherId = command.teacherId();
+        this.level = command.level();
+        this.duration = command.duration();
+        this.prior_knowledge = command.prior_knowledge();
+        this.principal_image = command.principal_image();
     }
+
+
 }
