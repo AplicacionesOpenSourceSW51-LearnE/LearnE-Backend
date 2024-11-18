d0df4bc5e9ccc7e90c7b7a093e57de1c9561ac9c
Pedro Guia <u202212010@upc.edu.pe>
2024-11-18 11:59:49 -0500

    feat(learne): update course enrollment table.

--- src/main/java/org/learne/platform/learne/domain/model/aggregates/CourseEnrollment.java
+++ src/main/java/org/learne/platform/learne/domain/model/aggregates/CourseEnrollment.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-import org.learne.platform.auth.domain.model.aggregates.User;
 import org.learne.platform.learne.domain.model.commands.CreateCourseEnrollmentCommand;
 import org.learne.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -14,11 +13,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class CourseEnrollment extends AuditableAbstractAggregateRoot<CourseEnrollment> {
     @Column
     @NotNull
-    private User student_id;
+    private Long student_id;
 
     @Column
     @NotNull
-    private Course course_id;
+    private Long course_id;
 
     protected CourseEnrollment() {}
 
