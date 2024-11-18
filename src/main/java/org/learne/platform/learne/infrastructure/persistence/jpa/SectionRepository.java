9bbf2301b9c6fa5f3d226d20faf9149d0f86e646
Pedro Guia <u202212010@upc.edu.pe>
2024-11-18 14:20:43 -0500

    feat(learne): added section repository.

---
+++ src/main/java/org/learne/platform/learne/infrastructure/persistence/jpa/SectionRepository.java
@@ -0,0 +1,14 @@
+package org.learne.platform.learne.infrastructure.persistence.jpa;
+
+import org.learne.platform.learne.domain.model.aggregates.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SectionRepository extends JpaRepository<Section, Long> {
+    Optional<Section> findByTitle(String title);
+
+    boolean existsByTitle(String title);
+}
