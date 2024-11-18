808c6a694007ba65341b968ccbe231de7ad892e9
Pedro Guia <u202212010@upc.edu.pe>
2024-11-18 12:42:41 -0500

    feat(learne): update course enrollment repository.

--- src/main/java/org/learne/platform/learne/infrastructure/persistence/jpa/CourseEnrollmentRepository.java
+++ src/main/java/org/learne/platform/learne/infrastructure/persistence/jpa/CourseEnrollmentRepository.java
@@ -4,9 +4,7 @@ import org.learne.platform.learne.domain.model.aggregates.CourseEnrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CourseEnrollmentRepository extends JpaRepository <CourseEnrollment, Long> {
-    Optional<CourseEnrollment> findCourseEnrollmentBy(Long student_id, Long course_id);
+    boolean findCourseEnrollmentBy(Long student_id, Long course_id);
 }
