1cec07902869bb55e2315626d3b7feae829c6a7e
Pedro Guia <u202212010@upc.edu.pe>
2024-11-19 18:45:31 -0500

    feat(learne): moved Section endpoint controller to rest.

--- src/main/java/org/learne/platform/learne/interfaces/rest/SectionController.java
+++ src/main/java/org/learne/platform/learne/interfaces/rest/SectionController.java
@@ -1,4 +1,4 @@
-package org.learne.platform.learne.interfaces.rest.transform;
+package org.learne.platform.learne.interfaces.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
