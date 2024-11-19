bb20baa4514c9caf2e5599494149bfd5a193ca67
Pedro Guia <u202212010@upc.edu.pe>
2024-11-19 18:27:10 -0500

    feat(learne): added the section class entity.

--- src/main/java/org/learne/platform/learne/domain/model/aggregates/Unit.java
+++ src/main/java/org/learne/platform/learne/domain/model/aggregates/Unit.java
@@ -1,16 +1,30 @@
 package org.learne.platform.learne.domain.model.aggregates;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
+import org.learne.platform.learne.domain.model.commands.CreateUnitCommand;
 import org.learne.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
 @Entity
 @Getter
 public class Unit extends AuditableAbstractAggregateRoot<Unit> {
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
 
+    @Column(nullable = false)
+    private String title;
 
     public Unit() {}
+
+    public Unit(CreateUnitCommand command) {
+        this.course = new Course(command.courseId());
+        this.title = command.title();
+    }
 
     //no borrar, se esta usando en Exam (borra este comentario)
     public Unit(Long id) {
