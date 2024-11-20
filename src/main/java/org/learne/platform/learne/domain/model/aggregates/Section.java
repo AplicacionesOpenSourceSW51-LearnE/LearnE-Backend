90723fccc2e360a2a9de0ab3cf885ae7162e9f7e
AnthonyHuapaya <u202220235@upc.edu.pe>
2024-11-19 23:24:12 -0500

    add Sections Endpoint

---
+++ src/main/java/org/learne/platform/learne/domain/model/aggregates/Section.java
@@ -0,0 +1,35 @@
+package org.learne.platform.learne.domain.model.aggregates;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import org.learne.platform.learne.domain.model.commands.Section.CreateSectionCommand;
+import org.learne.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+@Entity
+@Getter
+public class Section extends AuditableAbstractAggregateRoot<Section> {
+    @ManyToOne
+    @JoinColumn(name = "unit_id", nullable = false)
+    private Unit unit;
+    @Column(nullable = false)
+    @Size(max = 50)
+    private String title;
+    @Column(nullable = false)
+    @Size(max = 250)
+    private String description;
+    @Column(nullable = false)
+    private String url_video;
+
+    public Section() {}
+
+    public Section(CreateSectionCommand command) {
+        this.unit = new Unit(command.unitId());
+        this.title = command.title();
+        this.description = command.description();
+        this.url_video = command.urlVideo();
+    }
+}
